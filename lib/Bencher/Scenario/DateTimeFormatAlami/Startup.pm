2260f2d3e11da5450a74978040acb009000798ba
perlancar (@pc-office) <perlancar@gmail.com>
2016-04-06 20:48:59 +0700

    Initial commit

---
+++ lib/Bencher/Scenario/DateTimeFormatAlami/Startup.pm
@@ -0,0 +1,21 @@
+package Bencher::Scenario::DateTimeFormatAlami::Startup;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+our $scenario = {
+    summary => 'Benchmark startup of DateTime::Format::Alami against some other modules',
+    module_startup => 1,
+    participants => [
+        {module=>'DateTime::Format::Alami'},
+        {module=>'DateTime::Format::Natural'},
+        {module=>'DateTime'},
+    ],
+};
+
+1;
+# ABSTRACT:
---
+++ lib/Bencher/Scenarios/DateTimeFormatAlami.pm
@@ -0,0 +1,7 @@
+package Bencher::Scenarios::DateTimeFormatAlami;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: A collection of scenarios to benchmark DateTime::Format::Alami
